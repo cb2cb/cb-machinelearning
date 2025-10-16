0471f05e6a64cb41d98ed8eb463455bb4e53d361
cb2cb <159133715+cb2cb@users.noreply.github.com>
2025-10-16 19:21:59 +0200

    Remove owl emoji from app title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title('🦉Valora')
+st.title('Valora')
 
 df = pd.read_csv('https://raw.githubusercontent.com/cb2cb/cb-machinelearning/refs/heads/master/fundamental_data.csv')
 df
