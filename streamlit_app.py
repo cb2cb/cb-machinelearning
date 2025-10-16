c8d94ce14bf3dd57f5ca1845f51c4250bb8d7c0a
cb2cb <159133715+cb2cb@users.noreply.github.com>
2025-10-16 19:17:01 +0200

    Change title emoji from tree to owl

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title('🌳Valora')
+st.title('🦉Valora')
 
 df = pd.read_csv('https://raw.githubusercontent.com/cb2cb/cb-machinelearning/refs/heads/master/fundamental_data.csv')
 df
