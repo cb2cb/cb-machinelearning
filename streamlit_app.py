fad16d9a913b2a60646f97e2e520a0df4f8cb82b
cb2cb <159133715+cb2cb@users.noreply.github.com>
2025-10-16 19:11:35 +0200

    Fix title and add data loading functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
 
-st.title('🎈 Valora')
+st.title('Valora')
 
-st.write('')
+df = pd.read_csv('https://raw.githubusercontent.com/cb2cb/cb-machinelearning/refs/heads/master/fundamental_data.csv')
+df
