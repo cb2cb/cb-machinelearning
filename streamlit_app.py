4e79a7a998a299e18c40561be75c9e6e9fe86c38
cb2cb <159133715+cb2cb@users.noreply.github.com>
2025-10-16 19:15:47 +0200

    Add tree emoji to Valora title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Valora')
+st.title('🌳Valora')
 
 df = pd.read_csv('https://raw.githubusercontent.com/cb2cb/cb-machinelearning/refs/heads/master/fundamental_data.csv')
 df
